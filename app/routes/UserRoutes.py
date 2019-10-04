e2182be17862f0c8f45cd921ef0c17f9fd4c347a
Rashmi Ranjan Sahoo <54047909+rashmiranjanrrs@users.noreply.github.com>
2019-10-04 14:48:29 +0530

    Update UserRoutes.py

--- app/routes/UserRoutes.py
+++ app/routes/UserRoutes.py
@@ -4,13 +4,21 @@ from app.controller.UserController import UserController
 from flask import Blueprint
 
 userroutes = Blueprint("userroutes", __name__)
-
+# You can call your controller method here
 
 @userroutes.route('/signup', methods=['POST'])
 def signup():
-    return UserController.signup()
+    try:
+        if request.get_json()["username"] and request.get_json()["password"] and request.get_json()["email"]:
+            return UserController.signup()
+    except KeyError:
+        return jsonify({"error":"please add all data(username,password and email)"}), 500
+
+@userroutes.errorhandler(500)
+def error(e):
+    return "Input all the data(username,password,email)!! 500 error"
 
 @userroutes.errorhandler(400)
 def error(e):
-    return "check your input data properly!! 400 error"
+    return "check your input properly!! 400 error", 400
     
