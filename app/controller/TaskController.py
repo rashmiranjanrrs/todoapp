ee47f1cf326a175c9988f6ee38afbce208eaff0c
Rashmi Ranjan Sahoo <54047909+rashmiranjanrrs@users.noreply.github.com>
2019-10-10 09:00:29 +0530

    Update TaskController.py

--- app/controller/TaskController.py
+++ app/controller/TaskController.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from app.model.Task import Task
 from app.model.User import User
 from app.controller import auth
+from app import cache
 
 import datetime
 
@@ -13,11 +14,15 @@ class TaskController:
         content = data['content']
         task = Task(content, user=User.query.filter_by(username=auth.auth.username()).first())
         Task.addtask(task)
-        return jsonify({
+        task_id = task.id
+        task = jsonify({
             'username': auth.auth.username(),
-            'task-id': task.id,
+            'task_id': task.id,
             'content': task.content
         })
+        cache.set(task_id, task)
+        cache.delete('tasks')
+        return task
 
     def markdone():
         data = request.get_json()
@@ -51,17 +56,44 @@ class TaskController:
             'task_completed': task.done
         })
         Task.deletetask(task)
+        cache.flushall()
         return deleted_task
-
     def alltasks():
-        user = User.query.filter_by(username= auth.auth.username()).first()
-        if user is None:
-            return jsonify({
-                'status': 'failed'
+        task_list = cache.get('tasks')
+
+        if task_list == None or task_list =='':
+            user = User.query.filter_by(username=auth.auth.username()).first()
+            if user is None:
+                return jsonify({
+                    'status': 'failed'
+                })
+            task_list = {}
+            for task in user.tasks:
+                task_list[task.id] = {'content': task.content,
+                                      'add_date': task.add_date,
+                                      'task_completed': task.done}
+
+            cache.set('tasks', jsonify(task_list))
+
+            return jsonify(task_list)
+        return task_list
+
+    def onetask():
+        T = cache.get('task')
+        data = request.get_json()
+        task_id = data['task_id']
+        if T == None or T == '':
+            task = Task.query.filter_by(id=task_id).first()
+            if not task:
+                return jsonify({'message': 'No task found!'})
+
+            task = jsonify({
+                'task_id': task.id,
+                'content': task.content,
+                'task_completed': task.done
             })
-        task_list = {}
-        for task in user.tasks:
-            task_list[task.id] = {'content': task.content,
-                                  'add_date': task.add_date,
-                                  'task_completed': task.done}
-        return jsonify(task_list)
+            cache.set(task_id, task)
+            return task
+
+
+        return jsonify(T)
