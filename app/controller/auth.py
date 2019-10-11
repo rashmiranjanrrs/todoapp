d1c290aa82cc8b885fcdb07f8ae5555848723226
Rashmi Ranjan Sahoo <54047909+rashmiranjanrrs@users.noreply.github.com>
2019-10-11 13:35:44 +0530

    Update auth.py

--- app/controller/auth.py
+++ app/controller/auth.py
@@ -1,19 +1,11 @@
 from app.model.User import User
 from flask_httpauth import HTTPBasicAuth
-from app import cache
 
 auth = HTTPBasicAuth()
 @auth.get_password
 def pass_auth(username):
-    c_password = cache.get('password')
-    if c_password == '' or c_password is None:
-        user = User.query.filter_by(username=username).first()
-        if user is not None:
-            password = user.password
-            cache.set('password',password)
-            return password
-        else:
-            return None
-    
+    user = User.query.filter_by(username=username).first()
+    if user is not None:
+        return user.password
     else:
-        return c_password
+        return None
